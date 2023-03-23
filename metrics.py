d1f6e7241e39b32f42fbf743ac4eb58430f0c891
nathantgray <nathantgray@gmail.com>
2023-03-23 11:14:32 -0700

    update metrics.py with fairness index

--- metrics.py
+++ metrics.py
@@ -3,6 +3,26 @@ import numpy as np
 from pathlib import Path
 
 
+def parse_phasors(str_values: np.ndarray):
+    values = str_values.flatten()
+    n = values.shape[0]
+    for i in range(n):
+        values[i] = parse_phasor(values[i])
+    return values
+
+
+def parse_phasor(str_value: str):
+    if str_value[-1] != 'd':
+        raise ValueError("Value is not a phasor with angle in degrees.")
+    last_minus = str_value.rfind('-')
+    last_plus = str_value.rfind('+')
+    start_angle = max(last_plus, last_minus)
+    magnitude = float(str_value[:start_angle])
+    angle = float(str_value[start_angle:-1])
+    complex_value = magnitude*np.exp(np.radians(angle)*1j)
+    return complex_value
+
+
 def symmetrical_components(v_3ph):
     v_3ph_ = np.array([np.array(v_3ph).flatten()]).T
     a = np.exp(120*np.pi/180*1j)
@@ -55,7 +75,7 @@ if __name__ == '__main__':
     i_base = s_base / v_base
     v_df = pd.DataFrame()
     s_df = pd.DataFrame()
-    for i in range(0, 6):
+    for i in range(1, 6):
         # get_data(f"case{i}/support/output")
         v_support = pd.read_csv(Path(f"case{i}/support/output/output_voltage.csv"), header=1, index_col=0)
         v_support = process_v(v_support, v_base)
@@ -95,9 +115,39 @@ if __name__ == '__main__':
         s_df.loc[f'case{i}', 'q'] = q.sum()
         s_df.loc[f'case{i}_delta', 'q'] = q.sum() - q_no_support.sum()
 
+        # Fairness
+        s_rated = np.array([
+            [180000.0, 180000.0, 180000.0],  # Inverter 20
+            [195000.0, 195000.0, 195000.0],  # Inverter 23
+            [210000.0, 210000.0, 210000.0],  # Inverter 26
+            [225000.0, 225000.0, 225000.0],  # Inverter 29
+            [240000.0, 240000.0, 240000.0],  # Inverter 30
+        ])
+        p_out = np.array([
+            [75000, 75000, 75000],  # Inverter 20
+            [75000, 75000, 75000],  # Inverter 23
+            [75000, 75000, 75000],  # Inverter 26
+            [75000, 75000, 75000],  # Inverter 29
+            [75000, 75000, 75000],  # Inverter 30
+        ])
 
-
-
+        q_reserve = (s_rated ** 2 - p_out ** 2) ** 0.5
+        inv_df = pd.DataFrame()
+        inv_20 = pd.read_csv(Path(f"case{i}/support/output/inv_20_meter.csv"), header=8)
+        inv_23 = pd.read_csv(Path(f"case{i}/support/output/inv_23_meter.csv"), header=8)
+        inv_26 = pd.read_csv(Path(f"case{i}/support/output/inv_26_meter.csv"), header=8)
+        inv_29 = pd.read_csv(Path(f"case{i}/support/output/inv_29_meter.csv"), header=8)
+        inv_30 = pd.read_csv(Path(f"case{i}/support/output/inv_30_meter.csv"), header=8)
+        inv_df.loc['inv20', ['a', 'b', 'c']] = parse_phasors(inv_20.iloc[0, 1:].values)
+        inv_df.loc['inv23', ['a', 'b', 'c']] = parse_phasors(inv_23.iloc[0, 1:].values)
+        inv_df.loc['inv26', ['a', 'b', 'c']] = parse_phasors(inv_26.iloc[0, 1:].values)
+        inv_df.loc['inv29', ['a', 'b', 'c']] = parse_phasors(inv_29.iloc[0, 1:].values)
+        inv_df.loc['inv30', ['a', 'b', 'c']] = parse_phasors(inv_30.iloc[0, 1:].values)
+        loading = np.abs(inv_df.values.imag)/q_reserve
+        fairness = np.mean(np.sum(loading, axis=1))**2/np.mean(np.sum(loading, axis=1)**2)
+        # see origianl source https://www.cse.wustl.edu/~jain/papers/ftp/fairness.pdf
+        # see wikipedia source https://en.wikipedia.org/wiki/Fairness_measure
+        print(f"Case {i} Fairness={fairness}")
     print(v_df)
     print(s_df)
 
